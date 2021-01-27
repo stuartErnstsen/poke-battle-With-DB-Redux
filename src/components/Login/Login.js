a35abf4f99fc33d6816e9b5d3e9bb3f1fc4567c0
stuartErnstsen <stuarternstsen@gmail.com>
2021-01-26 22:13:55 -0700

    Finished styling for login nav and transition for login/reg buttons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setUser, removeUser, addTeam } from '../../ducks/userReducer';
 import axios from 'axios';
@@ -46,24 +46,36 @@ const Login = props => {
         }
     }
 
+    useEffect(() => {
+        if (!regView) {
+            document.getElementById('login-btn').classList.remove('button-hidden')
+            document.getElementById('reg-btn').classList.add('button-hidden')
+        } else {
+            document.getElementById('reg-btn').classList.remove('button-hidden-on-mount', 'button-hidden')
+            document.getElementById('login-btn').classList.add('button-hidden')
+        }
+
+    }, [regView])
+
     return (
         <header id="user-info">
             {props.user.loggedIn
                 ? (
-                    <div>
+                    <div className="user-info">
                         <h1>Trainer: {props.user.user.username}</h1>
                         <h2>Team name: {props.user.team?.team_name}</h2>
-                        <button onClick={handleLogout}>LOGOUT</button>
+                        <button className="alt-button" onClick={handleLogout}>LOGOUT</button>
                     </div>
                 ) : (
-                    <div>
-                        <div>
-                            <input value={input.usernameInput} name='usernameInput' onChange={handleChange} placeholder='Username' />
-                            <input value={input.passwordInput} name='passwordInput' onChange={handleChange} placeholder='Password' />
-                            {!regView && <button onClick={handleLogin}>LOGIN</button>}
-                            {regView && <button onClick={handleRegister}>REGISTER</button>}
-                        </div>
-                        <p>Not Registered? <span onClick={() => setRegView(!regView)}>Click here!</span></p>
+                    <div className="user-info">
+                        <input value={input.usernameInput} name='usernameInput' onChange={handleChange} placeholder='Username' />
+                        <input value={input.passwordInput} name='passwordInput' onChange={handleChange} placeholder='Password' />
+                        <button id='login-btn' onClick={handleLogin}>LOGIN</button>
+                        <button id='reg-btn' className="alt-button button-hidden-on-mount" onClick={handleRegister}>REGISTER</button>
+                        <p>{!regView ? <span>Not Registered? </span> : <span >Already Registered? </span>}
+                            <span className="span-hover" onClick={() => setRegView(!regView)}>Click here!
+                                </span>
+                        </p>
                     </div>
                 )
             }
